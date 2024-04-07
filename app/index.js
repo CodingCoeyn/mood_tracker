f8b9d0f2d7c767f7a8c9472e7ea89099007dd302
Coding Coeyn <codingcoeyn@gmail.com>
2024-04-07 00:46:50 -0400

    Code Review 3| Managed to display the array of records

--- app/index.js
+++ app/index.js
@@ -1,11 +1,11 @@
 const db = require('./models');
 const express = require('express');
-const axios = require('axios');
+// const axios = require('axios');
 const app = express();
 const cors = require("cors");
 
 app.use(cors());
-// app.use(express.json())
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Hello, Mahassin!');
--- app/src/components/Record_List.js
+++ app/src/components/Record_List.js
@@ -1,80 +1,58 @@
+import React, {Fragment, useEffect, useState} from "react";
 
-import React, {Component} from 'react';
-import axios from 'axios';
+const Record_List = () => {
 
-class Record_List extends Component{
-  // state = {
-  //   mood: 1
-  // };
-  state={
-    daily_records: []
-  }
-  componentDidMount(){
-    const url = "http://localhost:8000/daily_records";
-    // const url = "https://icanhazdadjoke.com";
-    const options = {
-      headers: {
-        Accept: "application/json"
-      }
+  const [daily_records, set_records] = useState([]);
+
+  const getAllRecords = async () =>{
+    try {
+      const response = await fetch("http://localhost:8000/daily_records");
+      const jsonData = await response.json();
+
+      console.log(jsonData);
+
+      set_records(jsonData);
+      
+    } catch (error) {
+      console.error(error.message);
     }
 
-    axios.get(url,options)
-    .then(response => {
-      console.log("response",response);
-      this.setState({ daily_records: response.data})
-      console.log("this.state",this.state.daily_records);
-    })
-    .catch(console.err);
   }
 
-  // render() {
-  //   const daily_records = this.state.daily_record;
-  //   console.log("render state",this.state.daily_record,"render records", daily_records);
-  //   console.log(typeof daily_records);
-  //   // this.test();
-
-  //   return (
-  //    <p></p>
-  //     // <div>
-  //     //   {/* <p>One a scale of 1-10</p> */}
-  //     //   <p>You are this happy: {daily_records}</p>
-  //     //   {/* <button onClick={this.increaseMood}>Cheer up!</button>
-  //     //   <button onClick={this.handleSetToTen}>Good Mood</button> */}
-  //     // </div>
-  //   )
-  // }
-  render(){
-    const daily_records = this.state.daily_records;
-    // console.log(daily_records[0]);
-    
-    return(
+  useEffect(() =>{
+    getAllRecords();
+  }, []);
+  return(
+    // console.log("return",daily_records)
+    <Fragment>
       <table>
         <thead>
           <tr>
-            <th>Date</th>
-            <th>Mood</th>
-            <th>Rating</th>
-            <th>Edit</th>
-            <th>Delete</th>
+              <th>Date</th>
+              <th>Mood</th>
+              <th>Rating</th>
+              <th>Edit</th>
+              <th>Delete</th>
           </tr>
         </thead>
         <tbody>
-
-        <tr id="daily_record[id]">
-          <td>daily_record[createdAt]</td>
-          <td>daily_record[mood]</td>
-          <td>daily_record[rating]</td>
-          <td><button>Edit</button></td>
-          <td><button>Delete</button></td>
-        </tr>
+          {
+            daily_records.map(daily_record => (
+              <tr key={daily_record.id}>
+                <td>{daily_record.createdAt}</td>
+                <td>{daily_record.mood}</td>
+                <td>{daily_record.ratingId}</td>
+                <td><button>Edit</button></td>
+                <td><button>Delete</button></td>
+              </tr>
+            ))
+          }
         </tbody>
-        
       </table>
-      // <p class="test-color"> I'm the Record List component</p>
-    );
-  }
+       
+    </Fragment>
+  );
 
 }
-
 
 export default Record_List;
