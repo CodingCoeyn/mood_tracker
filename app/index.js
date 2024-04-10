b97bea28394b3034667625caac9e695d3c9ac3cf
Coding Coeyn <codingcoeyn@gmail.com>
2024-04-10 19:20:45 -0400

    Changing machines

--- app/index.js
+++ app/index.js
@@ -1,8 +1,8 @@
-const db = require('./models');
-const express = require('express');
-// const axios = require('axios');
-const app = express();
 const cors = require("cors");
+const db = require('./models');
+const sequelize = require('sequelize');
+const express = require('express');
+const app = express();
 
 app.use(cors());
 app.use(express.json())
@@ -13,74 +13,109 @@ app.get('/', (req, res) => {
 
 // CREATE
 app.post('/daily_records', (req,res) => {
+    
+    try {
+        
+        db.daily_record.create({
+            mood: req.query.mood,
+            ratingId: parseInt(req.query.ratingId),
+            createdAt: new Date(),
+            updatedAt: new Date()
+        }).then(record_created=>{
+            res.send(record_created)
+        });
+    } catch (error) {
+        console.error(error.message);
+    }
 
-    db.daily_record.create({
-        mood: req.query.mood,
-        ratingId: parseInt(req.query.ratingId),
-        createdAt: new Date(),
-        updatedAt: new Date()
-    }).then(record_created=>{
-        res.send(record_created);
-        process.exit();
-    });
 
 });
 
 // READ
 app.get('/daily_records', (req,res) => {
     
-    db.daily_record.findAll().then(daily_records=>{
-        res.send(daily_records);
-    });
+    try {
+        db.daily_record.findAll().then(daily_records=>{
+            res.send(daily_records)
+        });
+        
+    } catch (error) {
+        console.error(error.message);
+    }
 
 });
 
 app.get('/daily_records/:id', (req,res) => {
 
-    db.daily_record.findOne({
-        where:{id: parseInt(req.params.id)}
-    }).then(found_record=>{
-        res.send(found_record);
-        process.exit();
-    });
+    try {
+        
+        db.daily_record.findOne({
+            where:{id: parseInt(req.params.id)}
+        }).then(found_record=>{
+            res.send(found_record)
+            
+        });
+    } catch (error) {
+        console.error(error.message);
+    }
+
 
 });
+
 
 // UPDATE
 
 app.put('/daily_records/:id', (req, res) => {
+
+    try {
+        
+        db.daily_record.update({
+            mood: req.query.mood,
+            ratingId: parseInt(req.query.ratingId),
+            updatedAt: new Date()
+        },{
+            where:{id: parseInt(req.params.id)}
+        }).then(records_changed=>{
+            res.send(records_changed+' records have been updated.')
+            // 
+        });
+    } catch (error) {
+        console.error(error.message);     
+    }
     
-    db.daily_record.update({
-        mood: req.query.mood,
-        ratingId: parseInt(req.query.ratingId),
-        updatedAt: new Date()
-    },{
-        where:{id: parseInt(req.params.id)}
-    }).then(records_changed=>{
-        res.send(records_changed+' records have been updated.');
-        // process.exit();
-    });
 
 });
 
 // DESTROY
 
 app.delete('/daily_records/:id', (req, res) => {
+
+    try {
+        db.daily_record.destroy({
+            where: {id: parseInt(req.params.id)}
+        }).then(records_deleted=>{
+            res.send(records_deleted+' records have been deleted.')
+            // 
+        });
+        
+    } catch (error) {
+        console.error(error.message);
+    }
     
-    db.daily_record.destroy({
-        where: {id: parseInt(req.params.id)}
-    }).then(records_deleted=>{
-        res.send(records_deleted+' records have been deleted.');
-        // process.exit();
-    });
 
 });
 
 app.delete('/daily_records', (req, res) => {
+
+    try {
+        
+        db.daily_record.truncate().then(deleted_records =>{
+            res.send("All records have been deleted.")
+        });
+    } catch (error) {
+        console.error(error.message);        
+    }
     
-    db.daily_record.truncate().then(deleted_records =>{
-        res.send("All records have been deleted.");
-    });
     
 });
 
--- app/src/components/Record_Modal.js
+++ app/src/components/Record_Modal.js
@@ -1,11 +1,11 @@
 
-import React, {Component} from 'react';
+import React, {Fragment} from 'react';
 
-class Record_Modal extends Component{
-
-  render(){
+const Record_Modal = () => {
     return(
       // modal fade
+      <Fragment>
+
       <div class=" col-6 border border-2 border-black" id="record_modal" tabindex="-1">
         <div class="modal-dialog">
           <div class="modal-content">
@@ -14,19 +14,21 @@ class Record_Modal extends Component{
               <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
             </div>
             <div class="modal-body text-start">
-              <label class=" fw-bold px-2" for="mood">Mood</label>
-              <input class="mb-2" type="text" id="mood" name="mood"></input>
-              <br></br>
-              <labl class=" fw-bold px-2" for="rating">Rating</labl>
-              <select class="mb-2" name="rating" id="rating">
-                <option value="">Great</option>
-                <option value="">Good</option>
-                <option value="">Fine</option>
-                <option value="">Okay</option>
-                <option value="">Blah</option>
-                <option value="">Unwell</option>
-                <option value="">Spiraling</option>
-              </select>
+              <form method="put" action="/">
+                <label class=" fw-bold px-2" for="mood">Mood</label>
+                <input class="mb-2" type="text" id="mood" name="mood"></input>
+                <br></br>
+                <labl class=" fw-bold px-2" for="rating">Rating</labl>
+                <select class="mb-2" name="rating" id="rating">
+                  <option value="">Great</option>
+                  <option value="">Good</option>
+                  <option value="">Fine</option>
+                  <option value="">Okay</option>
+                  <option value="">Blah</option>
+                  <option value="">Unwell</option>
+                  <option value="">Spiraling</option>
+                </select>
+              </form>
             </div>
             <div class="modal-footer m-2">
               <button type="button" class="btn btn-primary m-2">Save</button>
@@ -35,10 +37,10 @@ class Record_Modal extends Component{
           </div>
         </div>
       </div>
-      // <p class="test-color">I'm the Record Modal component</p>
+      </Fragment>
     );
   }
-
-}
+  
+  //{ <p class="test-color">I'm the Record Modal component</p>}
 
 export default Record_Modal;
