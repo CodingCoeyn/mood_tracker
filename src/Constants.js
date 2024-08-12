2281724ad7ff35b16894e1b45e738b7df6c1ee45
Coding Coeyn <codingcoeyn@gmail.com>
2024-08-12 02:23:49 -0400

    Remove development env

--- src/Constants.js
+++ src/Constants.js
@@ -6,4 +6,5 @@ const development = {
     url: 'http://localhost:8000'
 };
 
-export const config = process.env.NODE_ENV === 'development' ? development : production
+// export const config = process.env.NODE_ENV === 'development' ? development : production
+export const config = production;
